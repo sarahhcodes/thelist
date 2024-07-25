26836a55a33377e45279c489f1f5e92ae810db71
sarahhcodes <78184189+sarahhcodes@users.noreply.github.com>
2024-07-24 19:00:05 -0600

    adding css to template

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+import apsw
 
 app = Flask(__name__)
+connection = apsw.Connection("list.db", flags=apsw.SQLITE_OPEN_READWRITE)
+
+# TO DO:
+# finalize template design
+# login
+# populate list
 
 @app.route("/")
 
