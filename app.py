b3f1c700f6d1c2e38d18f5907292b02b2337d988
sarahhcodes <78184189+sarahhcodes@users.noreply.github.com>
2024-07-25 10:30:59 -0600

    adding watched link; updating to do list

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ connection = apsw.Connection("list.db", flags=apsw.SQLITE_OPEN_READWRITE)
 
 # TO DO:
 # finalize template design
+# change list to table
 # login
 # populate list
+# add links to each film -> where to watch popup; mark as watched
 
 @app.route("/")
 
