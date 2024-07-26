51161b7891238dc3ffc6166b975f6c6d852ea707
sarahhcodes <78184189+sarahhcodes@users.noreply.github.com>
2024-07-26 11:25:12 -0600

    starting login function

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
 import apsw
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 connection = apsw.Connection("list.db", flags=apsw.SQLITE_OPEN_READWRITE)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 
 # TO DO:
 # finalize template design
@@ -12,6 +16,9 @@ connection = apsw.Connection("list.db", flags=apsw.SQLITE_OPEN_READWRITE)
 # add links to each film -> where to watch popup; mark as watched
 
 @app.route("/")
-
 def index():
     return render_template("tempdesign.html")
+
+@app.route("/login")
+def login():
+    return "to do"
